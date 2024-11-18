a7a0871237cfe83b6f45a8018d70c3bbce49ed32
RSAPolyMathTechClub <projectNinjaz100@gmail.com>
2024-11-17 18:32:56 -0800

    first commit

---
+++ src/main/java/com/mebz/imspos/ImsposApplication.java
@@ -0,0 +1,13 @@
+package com.mebz.imspos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsposApplication.class, args);
+	}
+
+}
